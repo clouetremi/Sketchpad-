f4be34293827e09e799c0339c6e3613cae633517
Rémi Clouet <clouet.remi@gmail.com>
2025-01-13 22:03:44 +0100

    ajout du canvas afin de tracer la ligne continue

--- Sketchpad.js
+++ Sketchpad.js
@@ -24,20 +24,30 @@ container.addEventListener("mouseover", () => {
     console.log("la souris est visible")
 });
 
+// créer une variable qui va récupérer mon élément canvas
+const canvas = document.querySelector("canvas");
+// créer une variable qui définit le type de canvas (ici 2d)
+const ctx = canvas.getContext("2d");
 
+let isDrawing = false; 
+let lastX = 0;
+let lastY = 0;
 
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true; 
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // capturer la position de départ
+});
 
+canvas.addEventListener("mousemove", (e) => {
+    if (!isDrawing) return; // ne rien faire si la souris n'est pas enfoncée
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY); // départ de la ligne
+    ctx.lineTo(e.offsetX, e.offsetY); // destination de la ligne 
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // Mettre à jour la position 
+});
 
-
-// créer une fonction createDot qu'on va call dans nos events ci-dessus
-
-// function createDot(targetDiv, x, y) {
-//     const dot = document.createElement("div");
-//     dot.classList.add("dot");
-//     dot.style.position = "absolute";
-//     dot.style.left = `${x}`;
-//     dot.style.top = `${y}`;
-//     dot.style.left = `${x - targetDiv.getBoudingClientRect().left}px`;
-//     dot.style.top = `${y - targetDiv.getBoudingClientRect().top}px`;
-//     targetDiv.appendChild(dot);
-// };
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseleave", () => (isDrawing = false));
